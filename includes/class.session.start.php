55bf26eedf38a96ddfc37c0a731685b3227e0301
Alofoxx <alofoxx@gmail.com>
2016-11-07 14:02:28 -0800

    Start work on better session handling

--- includes/class.session.start.php
+++ includes/class.session.start.php
@@ -1,2 +1,16 @@
 <?php
-  session_start();
+//  session_start();
+$session = new SecureSessionHandler('cheese');
+
+ini_set('session.save_handler', 'files');
+session_set_save_handler($session, true);
+session_save_path(__DIR__ . '/sessions');
+$session->start();
+
+if ( ! $session->isValid(5)) {
+    $session->destroy();
+}
+
+$session->put('hello.world', 'bonjour');
+
+$session->get('hello.world'); // bonjour
